86b9d8e0c30fe3738f3201936bb1f89de705584f
Bwjohnson1005 <brandonwarrenjohnson@yahoo.com>
2025-01-13 18:39:55 -0600

    my commit

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,71 +1,64 @@
-// Create map object
-let myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 4
-});
+// Initialize the map
+const map = L.map('map').setView([0, 0], 2);
 
-// Add tile layer
+// Add the base tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+    maxZoom: 18,
+}).addTo(map);
 
-// Add legend
-let legend = L.control({ position: "bottomright" });
-
-legend.onAdd = function () {
-    let div = L.DomUtil.create("div", "info legend");
-    let limits = [0, 1, 2, 3, 4];
-    let colors = ['#FF0000', '#FF3300', '#FF9900', '#FFFF00', '#66FF00', '#00FF00']; // Example colors
-    let labels = ['Earthquake Depth'];
-
-    // Add the minimum and maximum.
-    let legendInfo = "<h1>Earthquakes: Past 7 Days</h1>";
-
-    div.innerHTML = legendInfo;
-
-    limits.forEach(function (limit, index) {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
-
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-};
-
-legend.addTo(myMap);
-
-// Create function to determine color based on quake depth
-function getColor(value) {
-    return value > 50 ? '#FF0000' :
-        value > 40 ? '#FF3300' :
-            value > 30 ? '#FF9900' :
-                value > 20 ? '#FFFF00' :
-                    value > 10 ? '#66FF00' :
-                        '#00FF00';
+// Function to get marker color based on depth
+function getColor(depth) {
+    return depth > 90 ? '#d73027' :
+           depth > 70 ? '#fc8d59' :
+           depth > 50 ? '#fee08b' :
+           depth > 30 ? '#d9ef8b' :
+           depth > 10 ? '#91cf60' :
+                        '#1a9850';
 }
 
-function addMarkers() {
-    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then((data) => {
-        console.log(data);
-        let quakes = data.features;
-        for (let i = 0; i < quakes.length; i++) {
-            // Define relevant quake qualities as variables
-            let geometry = quakes[i].geometry;
-            let coordinates = [geometry.coordinates[1], geometry.coordinates[0]];
-            let depth = geometry.coordinates[2]
-            let magnitude = quakes[i].properties.mag;
-            // Set color based on depth, radius based on magnitude, scale up for easy viewing
-            L.circle(coordinates, {
-                fillOpacity: 0.75,
-                color: getColor(depth),
-                fillColor: getColor(depth),
-                radius: ((magnitude * 20000))
-                // Add popups with quake variables
-            }).bindPopup(`<h1>${quakes[i].properties.place}</h1> <hr> 
-                <h3>Latitude: ${coordinates[0].toLocaleString()}</h3>
-                <h3>Longitude: ${coordinates[1].toLocaleString()}</h3>
-                <h3>Magnitude: ${magnitude.toLocaleString()}</h3>
-                <h3>Depth: ${depth.toLocaleString()}</h3>`).addTo(myMap);
+// Function to style markers based on magnitude and depth
+function styleMarker(feature) {
+    const magnitude = feature.properties.mag;
+    const depth = feature.geometry.coordinates[2];
+    return {
+        radius: magnitude * 2, // Scale radius by magnitude
+        fillColor: getColor(depth), // Color by depth
+        color: '#000', // Black outline
+        weight: 1, // Border thickness
+        opacity: 1,
+        fillOpacity: 0.8,
+    };
+}
+
+// Load the earthquake data
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(data => {
+    // Add GeoJSON layer to the map
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, styleMarker(feature)); // Use circle markers
+        },
+        onEachFeature: function (feature, layer) {
+            // Add popup to each feature
+            layer.bindPopup(`<h3>Magnitude: ${feature.properties.mag}</h3>
+                             <p>Depth: ${feature.geometry.coordinates[2]} km</p>
+                             <p>Location: ${feature.properties.place}</p>`);
         }
-    });
-}
-addMarkers()
+    }).addTo(map);
+
+    // Add legend to the map
+    const legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function () {
+        const div = L.DomUtil.create('div', 'info legend');
+        const depths = [0, 10, 30, 50, 70, 90];
+
+        div.innerHTML += '<h4>Depth (km)</h4>';
+        depths.forEach((depth, i) => {
+            div.innerHTML += `
+                <i style="background:${getColor(depth)}"></i>
+                ${depth}${depths[i + 1] ? `&ndash;${depths[i + 1]}<br>` : '+'}`;
+        });
+
+        return div;
+    };
+    legend.addTo(map);
+}).catch(err => console.error('Error loading GeoJSON data:', err));
